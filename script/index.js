4cb4c36c7bf530a347d270e585eea50bba98cb0e
Nazmul0197 <sirmd97@gmail.com>
2026-09-23 22:39:17 +0600

    trees category api load and show

--- script/index.js
+++ script/index.js
@@ -1,1 +1,22 @@
 //
+const treesCatagory = document.getElementById("treesCatagory");
+
+const loadCatagory = async () => {
+  const url = `https://openapi.programming-hero.com/api/categories`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    showCategory(data.categories);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const showCategory = (categorise) => {
+  categorise.forEach((cateory) => {
+    treesCatagory.innerHTML += `
+           <li id="${cateory.id}" class="p-2 rounded-md text-[#1F2937] hover:bg-[#15803D] hover:text-white hover:cursor-pointer">${cateory.category_name}</li>  
+    `;
+  });
+};
+
+loadCatagory();
